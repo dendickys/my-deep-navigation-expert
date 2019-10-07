6ffe135faa93ac11b02890ac82f8063091361171
dendickys <febri.unquad2014@gmail.com>
2019-10-08 05:52:05 +0700

    Add DetailActivity

--- app/src/main/java/com/dendickys/mydeepnavigationexpert/MainActivity.java
+++ app/src/main/java/com/dendickys/mydeepnavigationexpert/MainActivity.java
@@ -2,13 +2,29 @@ package com.dendickys.mydeepnavigationexpert;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOpenDetail = findViewById(R.id.btn_open_detail);
+        btnOpenDetail.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_open_detail) {
+            Intent detailIntent = new Intent(MainActivity.this, DetailActivity.class);
+            detailIntent.putExtra(DetailActivity.EXTRA_TITLE, getString(R.string.detail_title));
+            detailIntent.putExtra(DetailActivity.EXTRA_MESSAGE, getString(R.string.detail_message));
+            startActivity(detailIntent);
+        }
     }
 }
